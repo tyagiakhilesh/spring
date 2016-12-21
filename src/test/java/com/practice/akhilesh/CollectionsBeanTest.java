3ef3b9151f2998b04f281dd2a1b3b337b155fda5
Akhilesh Tyagi <Akhilesh.Tyagi@hpe.com>
2016-12-21 12:23:29 +0530

    More assertions

--- src/test/java/com/practice/akhilesh/CollectionsBeanTest.java
+++ src/test/java/com/practice/akhilesh/CollectionsBeanTest.java
@@ -9,8 +9,13 @@ public class CollectionsBeanTest {
 
 	@Test
 	public void testACollectionBeanCreation() {
+		@SuppressWarnings("resource")
 		ApplicationContext aContext = new ClassPathXmlApplicationContext("resources/collections.test.xml");
 		ABeanHavingCollections aBean = aContext.getBean("aBeanHavingCollections", ABeanHavingCollections.class);
 		Assert.assertEquals(aBean.name, "akhilesh", "Value should be 'akhilesh'");
+		Assert.assertTrue(aBean.aMap.containsKey("firstMapKey"));
+		Assert.assertTrue(aBean.aMap.containsKey("secondMapKey"));
+		Assert.assertTrue(aBean.aMap.get("firstMapKey").equals("firstMapKeyValue"));
+		Assert.assertTrue(aBean.aMap.get("secondMapKey").equals("secondMapKeyValue"));
 	}
 }
